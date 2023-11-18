bbed5e971ec1c7231dfd6b22d39d6b3943199609
vicberger7 <vicberger7@gmail.com>
2023-11-18 18:01:11 +0100

    hw2-789

--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,6 @@
 function checkForSpam(message) {
-    const uppercaseMessage = message.toUpperCase();
-    const lowercaseMessage = message.toLowerCase();
-
-    return (
-        uppercaseMessage.includes('SPAM') || 
-        uppercaseMessage.includes('SALE') ||
-        lowercaseMessage.includes('spam') ||
-        lowercaseMessage.includes('sale')
-    );
+    const normalizedMessage = message.toLowerCase();
+    return normalizedMessage.includes('spam') || normalizedMessage.includes('sale');
 }
+  
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
